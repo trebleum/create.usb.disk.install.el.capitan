f0846f678615e7ed7e7ea2f572e6b80b32ce062a
Samuel C <samuel+github@chabert.org>
2015-10-07 17:31:22 +0200

    cosmetic+warning

--- README.md
+++ README.md
@@ -1,8 +1,7 @@
-# create.usb.disk.install.el.capitan
-=======
 # HOWTO
+=======
 
-1. Download El Capitan Installer via Mac App Store (DON'T INSTALL IT)
+1. Download El Capitan Installer via Mac App Store (**DON'T INSTALL IT**)
 2. Identify your USB device with `diskutil list`
 3. Launch script in Terminal : `sh createUSBDiskElCapitan.sh`
 
--- createUSBDiskElCapitan.sh
+++ createUSBDiskElCapitan.sh
@@ -1,6 +1,7 @@
-!#/bin/sh
+#!/bin/sh
 
-echo "id du disk USB" 
+echo "WARNING: an error on disk's ID could be fatal !"
+echo "id disk USB (obtain with diskutil list)" 
 read usbdiskN
 diskutil partitionDisk $usbdiskN 1 GPT HFS+ newdisk R
 sudo /Applications/Install\ OS\ X\ El\ Capitan.app/Contents/Resources/createinstallmedia --volume /Volumes/newdisk --applicationpath /Applications/Install\ OS\ X\ El\ Capitan.app/ --nointeraction
